2004b2e38c86873f9c43446332207d399aded828
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-11-19 20:03:37 +0700

    cors update

--- jshoes-client/src/interceptors/axiosInstance.ts
+++ jshoes-client/src/interceptors/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8000", // Backend base URL
+  withCredentials: true,
   // baseURL: "http://0.0.0.0:8000", // Backend base URL
 });
 
