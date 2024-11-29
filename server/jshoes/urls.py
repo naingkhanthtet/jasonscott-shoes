44ae7c2452a1358666d370fecf15c103be65a831
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-11-29 15:11:48 +0700

    admin url

--- server/jshoes/urls.py
+++ server/jshoes/urls.py
@@ -6,9 +6,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from decouple import config
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(config("DJANGO_ADMIN_URL"), admin.site.urls),
     path("auth/", include("users.urls")),
     path("shop/", include("shop.urls")),
 ]
