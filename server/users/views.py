b3de48de049f3dc2703738537c6c0d18cec88aa9
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-02 18:58:16 +0700

    removing imports

--- server/users/views.py
+++ server/users/views.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
