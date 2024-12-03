3fa4ed2b4eaa273365d52ecd8bd65933edbffe28
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-03 16:35:44 +0700

    urls for JWT

--- server/jshoes/urls.py
+++ server/jshoes/urls.py
@@ -6,12 +6,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from decouple import config
 
 urlpatterns = [
     path(config("DJANGO_ADMIN_URL"), admin.site.urls),
     path("auth/", include("users.urls")),
     path("shop/", include("shop.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 if settings.DEBUG:
--- server/users/urls.py
+++ server/users/urls.py
@@ -2,20 +2,16 @@ from django.urls import path
 from .views import (
     register_view,
     login_view,
-    logout_view,
     get_user,
     sync_user_data,
-    csrf_token_view,
     fetch_user_data,
     change_password,
     delete_user,
 )
 
 urlpatterns = [
-    path("csrf/", csrf_token_view, name="csrf_token_view"),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
     path("user/", get_user, name="get_user"),
     path("sync/", sync_user_data, name="sync_user_data"),
     path("user-data/", fetch_user_data, name="fetch_user_data"),
