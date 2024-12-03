c6f766538aa25ea9f9c080d0fb9170393d28f77b
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-03 16:32:57 +0700

    new axios for JWT

--- jshoes-client/src/interceptors/axiosInstance.ts
+++ jshoes-client/src/interceptors/axiosInstance.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const baseUrl = import.meta.env.VITE_LOCAL_BACKEND;
+// const baseUrl = import.meta.env.VITE_BACKEND_URL;
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
-  // baseURL: import.meta.env.VITE_LOCAL_BACKEND,
-  withCredentials: true,
+  baseURL: baseUrl,
+  // withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
@@ -11,6 +13,10 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
     return config;
   },
   (error) => {
@@ -19,12 +25,34 @@ axiosInstance.interceptors.request.use(
 );
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
+  (response) => response,
+  async (error) => {
     if (error.response && error.response.status === 401) {
-      console.log("Unauthorized! Redirecting to login...");
+      const refreshToken = localStorage.getItem("refresh_token");
+      if (refreshToken) {
+        try {
+          const response = await axios.post(
+            `${baseUrl}/api/token/refresh/`,
+            {
+              refresh: refreshToken,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const { access } = response.data;
+          localStorage.setItem("access_token", access);
+          error.config.headers["Authorization"] = `Bearer ${access}`;
+          return axiosInstance.request(error.config);
+        } catch (err) {
+          console.error("Token refresh error", err);
+          localStorage.removeItem("access_token");
+          localStorage.removeItem("refresh_token");
+          alert("Please log in again. Session expired.");
+        }
+      }
     }
     return Promise.reject(error);
   }
